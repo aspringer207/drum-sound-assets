a8bb4f6306a709668d13aa86e6da9f4c00a53fe0
Andre <andrea.m.springer@gmail.com>
2024-02-24 22:28:59 -0500

    makeupdates
    Signed-off-by: Andre <andrea.m.springer@gmail.com>

--- Public/index.js
+++ Public/index.js
@@ -1,16 +1,26 @@
-const sticks = document.getElementById('q');
-const hiHat = document.getElementById('w');
-const crash = document.getElementById('e');
-const ride = document.getElementById('a');
-const tomLo = document.getElementById('s');
-const tomMid = document.getElementById('d');
-const tomHi = document.getElementById('z');
-const bass = document.getElementById('x');
-const snare = document.getElementById('c')
+const sticks = document.getElementById("q");
+const hihat = document.getElementById("w");
+const crash = document.getElementById("e");
+const ride = document.getElementById("a");
+const lotom = document.getElementById("s");
+const midtom = document.getElementById("d");
+const hitom = document.getElementById("z");
+const bass = document.getElementById("x");
+const snare = document.getElementById("c");
 
-const tracks = [sticks, hiHat, crash, ride, tomLo, tomMid, tomHi, snare, bass]
-function playClip(el){
-    const audioElement = new Audio(el.src);
-    document.addEventListener('click', audioElement.play)
+const tracks = [
+	{ src: document.getElementById("sticks"), btn: document.getElementById("q") },
+	{ src: document.getElementById("hihat"), btn: document.getElementById("w") },
+	{ src: document.getElementById("crash"), btn: document.getElementById("e") },
+	{ src: document.getElementById("ride"), btn: document.getElementById("a") },
+	{ src: document.getElementById("lotom"), btn: document.getElementById("s") },
+	{ src: document.getElementById("midtom"), btn: document.getElementById("d") },
+	{ src: document.getElementById("hitom"), btn: document.getElementById("z") },
+	{ src: document.getElementById("bass"), btn: document.getElementById("x") },
+	{ src: document.getElementById("snare"), btn: document.getElementById("c") },
+];
+function player(el) {
+	const source = document.getElementById(el);
+    const str = 
+	
 }
-
